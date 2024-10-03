4e43a72efaa006195a6ce717a5716b0765651997
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-03 00:41:56 -0300

    fix(leetCode): fix challenge - Two Sum

--- src/main/java/com/studies/leetocde/twoSum/Solution.java
+++ src/main/java/com/studies/leetocde/twoSum/Solution.java
@@ -1,6 +1,8 @@
 package com.studies.leetocde.twoSum;
 
 
+import com.studies.utils.UseFullMethods;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,16 +12,18 @@ class Result{
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int [] result = new int[2];
-        System.out.println(Arrays.toString(nums));
+
+        UseFullMethods.printArray(nums);
 
         for(int i = 0; i < nums.length; i++){
             if(map.containsKey(nums[i])){
                 result[0] = i;
                 result[1] = map.get(nums[i]);
+                break;
             }
-            map.put( Math.abs(target - nums[i]), i);
+            map.put( target - nums[i], i);
+            UseFullMethods.printMap(map);
         }
-        map.forEach((k, v) -> System.out.println("key: " + k + " value: " + v));
         return result;
     }
 }
@@ -30,15 +34,18 @@ public class Solution {
 //        int[] nums = new int[]{2,7,11,15};
 //        int target = 9;
 
+//        int[] nums = new int[]{10,7,-1,15};
+//        int target = 9;
+
 //        int[] nums = new int[]{3,2,4};
 //        int target = 6;
 
 
         int[] nums = new int[]{-1, 3, 3, -3};
-        int target = 4;
+        int target = -4;
 
         int[] twoSum = Result.twoSum(nums, target);
 
-        System.out.println(Arrays.toString(twoSum));
+        UseFullMethods.printArray(twoSum);
     }
 }
